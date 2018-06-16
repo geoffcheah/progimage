d347916c31d8ded066dc650267ad4e0b0d44ea8a
Geoff Cheah <geoffcheah@gmail.com>
2018-06-16 22:55:45 +0100

    added code to allow for successful POST action upload

--- app/controllers/api/v1/pictures_controller.rb
+++ app/controllers/api/v1/pictures_controller.rb
@@ -7,10 +7,27 @@ class Api::V1::PicturesController < Api::V1::BaseController
 
   def create
     @picture = Picture.new(name: params["name"], description: params["description"])
-    puts current_user.email
     @picture.user = current_user
-    authorize @picture
+    uploaded_io = params["picture"]
+    aws_image_key = uploaded_io.original_filename
+    image_to_upload = uploaded_io.tempfile
 
+    s3 = Aws::S3::Resource.new(region:'eu-west-1')
+    obj = s3.bucket("progimage30").object("image_uploads/#{aws_image_key}")
+
+    File.open(image_to_upload, 'rb') do |file|
+      obj.put(body: file)
+    end
+
+    base_url = "https://s3-eu-west-1.amazonaws.com/progimage30/image_uploads/"
+    @picture.remote_url = "https://s3-eu-west-1.amazonaws.com/progimage30/image_uploads/#{params["name"]}"
+
+    authorize @picture
+    if @picture.save
+      render :show, status: :created
+    else
+      render_error
+    end
   end
 
 
