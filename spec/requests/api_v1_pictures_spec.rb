981ced67c877d26f76c20b8858e33db3c96768ee
Geoff Cheah <geoffcheah@gmail.com>
2018-06-16 16:58:00 +0100

    Added a test spec for the GET pictures/:id api end point

---
+++ spec/requests/api_v1_pictures_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+require 'api/v1/pictures_controller'
+require 'json'
+
+describe "Pictures API" do
+  hash_for_json = { name: "IMG_8394.JPG",
+      description: "some description",
+      remote_url: "https://s3-eu-west-1.amazonaws.com/progimage30/image_uploads/IMG_8394.JPG"
+    }
+
+  json = JSON.generate(hash_for_json)
+  let(:parsed_json) { JSON.parse(json) }
+
+
+  context "when calling the GET pictures/:id (show) endpoint or returning newly created image information" do
+    it "returns a json with the picture's key information" do
+      get '/api/v1/pictures/:id'
+      expect(response).to be_success
+      expect(parsed_json["name"]).to eq("IMG_8394.JPG")
+      expect(parsed_json["remote_url"]).to eq("https://s3-eu-west-1.amazonaws.com/progimage30/image_uploads/IMG_8394.JPG")
+    end
+  end
+
+end
