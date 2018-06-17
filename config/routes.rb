bcbddf576ecdd1e779cb8db6337f4a3fb04f0dd8
Geoff Cheah <geoffcheah@gmail.com>
2018-06-17 17:40:00 +0100

    added front end landing page and ability for users to sign up and view their dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  get 'dashboard', to: "pages#dashboard"
+
   namespace :api, defaults: { format: :json }  do
     namespace :v1 do
       resources :pictures, only: [ :show, :create ]
