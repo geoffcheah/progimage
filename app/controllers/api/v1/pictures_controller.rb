20b03001e8b224323baff1e71784571bd85262ee
Geoff Cheah <geoffcheah@gmail.com>
2018-06-17 12:12:58 +0100

    extrapolated logic for uploading the images to s3 into a service class object

--- app/controllers/api/v1/pictures_controller.rb
+++ app/controllers/api/v1/pictures_controller.rb
@@ -12,22 +12,19 @@ class Api::V1::PicturesController < Api::V1::BaseController
     aws_image_key = uploaded_io.original_filename
     image_to_upload = uploaded_io.tempfile
 
-    s3 = Aws::S3::Resource.new(region:'eu-west-1')
-    obj = s3.bucket("progimage30").object("image_uploads/#{aws_image_key}")
+    UploadToS3Service.new(aws_image_key, image_to_upload).call
 
-    File.open(image_to_upload, 'rb') do |file|
-      obj.put(body: file)
-    end
-
-    base_url = "https://s3-eu-west-1.amazonaws.com/progimage30/image_uploads/"
     @picture.remote_url = "https://s3-eu-west-1.amazonaws.com/progimage30/image_uploads/#{params["name"]}"
-
     authorize @picture
     if @picture.save
       render :show, status: :created
     else
       render_error
     end
+  end
+
+  def convert
+    @picture = Picture.find(params[:id])
   end
 
 
---
+++ app/services/upload_to_s3_service.rb
@@ -0,0 +1,15 @@
+class UploadToS3Service
+  def initialize(aws_image_key_name, image_file)
+    @image_key_name = aws_image_key_name
+    @image = image_file
+  end
+
+  def call
+    s3 = Aws::S3::Resource.new(region:'eu-west-1')
+    obj = s3.bucket("progimage30").object("image_uploads/#{@image_key_name}")
+
+    File.open(@image, 'rb') do |file|
+      obj.put(body: file)
+    end
+  end
+end
