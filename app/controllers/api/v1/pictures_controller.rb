33851e440967d955b000f3c3fcbfd961dd98af77
Geoff Cheah <geoffcheah@gmail.com>
2018-06-18 17:59:55 +0100

    fixed the controller tests for the show action

--- app/controllers/api/v1/pictures_controller.rb
+++ app/controllers/api/v1/pictures_controller.rb
@@ -7,7 +7,8 @@ class Api::V1::PicturesController < Api::V1::BaseController
   end
 
   def create
-    @picture = Picture.new(name: params["name"], description: params["description"])
+    # @picture = Picture.new(name: params["name"], description: params["description"])
+    @picture = Picture.new(picture_params)
     @picture.user = current_user
     uploaded_io = params["picture"]
     aws_image_key = uploaded_io.original_filename
@@ -25,15 +26,16 @@ class Api::V1::PicturesController < Api::V1::BaseController
   end
 
   def convert
-    @picture = Picture.find(params["id"].to_i)
-    image_url = @picture.remote_url
+    picture = Picture.find(params["id"].to_i)
+    image_url = picture.remote_url
     new_format = params["convert_to"]
-    old_image_name_without_type = @picture.name.slice(/.*(?=\.)/)
+    old_image_name_without_type = picture.name.slice(/.*(?=\.)/)
     new_image_name = "#{old_image_name_without_type}.#{new_format}"
     image_to_upload = ConvertImageService.new(image_url, new_format).call
     UploadToS3Service.new(new_image_name, image_to_upload).call
-    @picture = Picture.new(name: new_image_name, description: @picture.description, user: @picture.user )
+    @picture = Picture.new(name: new_image_name, description: picture.description, user: picture.user )
     @picture.remote_url = "https://s3-eu-west-1.amazonaws.com/progimage30/image_uploads/#{new_image_name}"
+    authorize @picture
     if @picture.save
       render :show, status: :created
     else
@@ -49,7 +51,7 @@ class Api::V1::PicturesController < Api::V1::BaseController
       status: :unprocessable_entity
   end
 
-  # def picture_params
-  #   params.require(:picture).permit(:name, :description, :picture)
-  # end
+  def picture_params
+    params.permit(:name, :description)
+  end
 end
--- spec/controllers/api_v1_pictures_controller_spec.rb
+++ spec/controllers/api_v1_pictures_controller_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
+require 'pry-byebug'
+
 require 'api/v1/pictures_controller'
 
 RSpec.describe Api::V1::PicturesController, :type => :controller do
@@ -9,6 +11,12 @@ RSpec.describe Api::V1::PicturesController, :type => :controller do
       description: "some description",
       remote_url: "https://s3-eu-west-1.amazonaws.com/progimage30/image_uploads/IMG_8394.JPG",
       user: user
+    }
+  end
+
+  let(:pic_attributes_in_form_params) do
+    { name: "IMG_8394.JPG",
+      description: "some description"
     }
   end
 
@@ -24,19 +32,19 @@ RSpec.describe Api::V1::PicturesController, :type => :controller do
 
     it "assigns @picture to the requested picture" do
       picture = Picture.create!(valid_attributes)
-      get :show, params: { :id => picture.id}
+      get :show, format: :json, params: { :id => picture.id }
       expect(assigns(:picture)).to eq(picture)
     end
 
     it "returns a json template" do
       picture = Picture.create!(name: "some_name.jpg", remote_url: "some_url", user: user)
-      get :show, params: { :id => picture.id }
+      get :show, format: :json, params: { :id => picture.id }
       expect(response).to render_template(:show)
     end
 
     it "returns a 200 OK status" do
       picture = Picture.create!(valid_attributes)
-      get :show, params: { :id => picture.to_param }
+      get :show, format: :json, params: { :id => picture.id }
       expect(response).to have_http_status(:ok)
     end
   end
@@ -46,37 +54,37 @@ RSpec.describe Api::V1::PicturesController, :type => :controller do
 
     context "with valid params" do
       it "creates a new picture" do
-        picture = Picture.create!(valid_attributes)
+        sign_in(user)
         expect {
-          p post :create, params: { :picture => valid_attributes}
+          post :create, format: :json, params: { :picture => valid_attributes }
         }.to change(Picture, :count).by(1)
       end
 
       it "assigns a newly created picture as @picture" do
-        post :create, params: { :picture => valid_attributes }
+        post :create, format: :json, params: { :picture => valid_attributes }
         expect(assigns(:picture)).to be_a(Picture)
         expect(assigns(:picture)).to be_persisted
       end
 
       it "returns a 201 created status" do
-        post :create
+        post :create, format: :json
         expect(response).to have_http_status(:created)
       end
 
       it "renders the json template for an individual picture (show)" do
-        post :create, params: { :picture => valid_attributes }
+        post :create, format: :json, params: { :picture => valid_attributes }
         expect(response).to render_template(:show)
       end
     end
 
     context "with invalid params" do
       it "sends back an error" do
-        post :create, params: { :picture => invalid_params }
+        post :create, format: :json, params: { :picture => invalid_params }
         expect(response).to render_template(:error)
       end
 
       it "returns a 422 error status" do
-        post :create, params: { :picture => invalid_params }
+        post :create, format: :json, params: { :picture => invalid_params }
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
